fa95648e9c8babc02e73d09d4de12f85460a5b59
wariza <wjarizatruquetkd@gmail.com>
2018-01-05 21:02:24 -0500

    Serverjs
    Corrections done for database connection

---
+++ server.js
@@ -0,0 +1,26 @@
+// dependencies
+   var express  = require('express');
+   var app      = express();
+   var mongoose = require('mongoose');
+   var morgan = require('morgan');
+   var bodyParser = require('body-parser');
+   var methodOverride = require('method-override');
+   var bodyParser = require('body-parser');
+    var methodOverride = require('method-override');
+    var database = require('./config/database');
+    var port     = process.env.PORT || 8888;
+
+   // configuration =================
+
+  mongoose.connect(database.url); 
+
+   app.use(express.static(__dirname + '/public'));
+   app.use(morgan('dev'));
+   app.use(bodyParser.urlencoded({'extended':'true'}));
+   app.use(bodyParser.json());
+   app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+   app.use(methodOverride());
+
+
+   app.listen(8080);
+   console.log("App listening on port 8080");
