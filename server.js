7c5217e38b14895087e04509ffe02d7201b23465
wariza <wjarizatruquetkd@gmail.com>
2018-01-05 21:21:59 -0500

    frontend
    Configure frontEnd

--- server.js
+++ server.js
@@ -30,8 +30,7 @@
        // get all todos
        app.get('/api/todos', function(req, res) {
 
-           // use mongoose to get all todos in the database
-           Todo.find(function(err, todos) {
+             Todo.find(function(err, todos) {
 
                // if there is an error retrieving, send the error. nothing after res.send(err) will execute
                if (err)
@@ -78,3 +77,9 @@
                });
            });
        });
+
+
+       // application -------------------------------------------------------------
+   app.get('*', function(req, res) {
+       res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+   });
